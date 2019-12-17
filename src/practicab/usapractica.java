da6cb6d8d07e53a802b218b54a8529943d7b17f3
miguel <miguel@taco.mshome.net>
2019-12-17 08:47:45 -0500

    holaaa

---
+++ src/practicab/usapractica.java
@@ -0,0 +1,13 @@
+
+package practicab;
+
+
+public class usapractica {
+    public static void main(String[] args) {
+        practica1 p=new practica1();
+        p.setVisible(true);
+        p.setLocationRelativeTo(null);
+                
+                
+    }
+}
