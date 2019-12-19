cc16700cde78623cccf68661356982e4db106bbf
zhagui <zhagui@Bryan>
2019-12-19 08:35:09 -0500

    como estas

--- src/practicab/practica1.java
+++ src/practicab/practica1.java
@@ -80,7 +80,7 @@ public class practica1 extends javax.swing.JFrame {
 
     private void btnactivarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnactivarActionPerformed
        lblmensaje.setText("hola..");
-       lblmensaje2.setText("como te va...");
+       lblmensaje2.setText("como estas...");
         
     }//GEN-LAST:event_btnactivarActionPerformed
 
